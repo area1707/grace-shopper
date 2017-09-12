b5d42e03c78173515295023dc8967816215100ff
An Le <lean@Ans-MacBook-Air.local>
2017-09-12 11:35:03 -0400

    add temp navbar to test google OAuth

--- client/components/Main.jsx
+++ client/components/Main.jsx
@@ -1,13 +1,14 @@
 import React, {Component} from 'react'
 import {Route, BrowserRouter as Router, Switch} from 'react-router-dom'
 import store from '../store'
+import Navbar from './Navbar'
 
 export default class Main extends Component {
   render() {
     return(
       <Router>
         <div>
-          Tabby Cats
+          <Navbar />
         </div>
       </Router>
     )
---
+++ client/components/Navbar.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { Link, NavLink, withRouter } from 'react-router-dom';
+import axios from 'axios';
+
+/* -----------------    COMPONENT     ------------------ */
+
+class Navbar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.renderLoginSignup = this.renderLoginSignup.bind(this);
+    this.renderLogout = this.renderLogout.bind(this);
+  }
+
+  render() {
+    return (
+      <nav>
+        <div>
+          { this.renderLogout() }
+          { this.renderLoginSignup() }
+        </div>
+      </nav>
+    )
+  }
+
+  renderLoginSignup() {
+    return (
+      <ul className="nav navbar-nav navbar-right">
+        <li>
+          <button>
+            <NavLink to="/signup" activeClassName="active">signup</NavLink>
+          </button>
+        </li>
+        <li>
+          <button>
+            <NavLink to="/login" activeClassName="active">login</NavLink>
+          </button>
+        </li>
+      </ul>
+    );
+  }
+
+  renderLogout() {
+    return (
+      <ul className="nav navbar-nav navbar-right">
+        <li>
+        <button
+          className="navbar-btn btn btn-default"
+          onClick={this.props.logout}>
+          logout
+        </button>
+        </li>
+      </ul>
+    );
+  }
+}
+
+/* -----------------    CONTAINER     ------------------ */
+
+const mapProps = null;
+
+const mapDispatch = dispatch => ({
+  logout: () => {
+    axios.get('/logout')
+    .then(() => {
+      console.log('You signed out.');
+    })
+    this.props.history.push('/');
+  }
+});
+
+export default withRouter(connect(mapProps, mapDispatch)(Navbar));
