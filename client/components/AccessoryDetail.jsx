c2c1045b847f93b707607037a2215485c2de18aa
Erin Shaw <erin.kimberly.shaw@gmail.com>
2017-09-13 17:38:38 -0400

    fix: removed console logs

--- client/components/AccessoryDetail.jsx
+++ client/components/AccessoryDetail.jsx
@@ -4,24 +4,18 @@ import {withRouter} from 'react-router'
 import SingleAccessory from './SingleAccessory'
 
 function Accessory(props) {
+
   const accessoryId =  props.match.params.accessoryId
   const accessories = props.accessories
   const accessory = accessories.find((acc) => acc.id === +accessoryId)
-  console.log('accessories', accessories)
-  console.log('accessoryId', accessoryId)
-  console.log('accessory', accessory)
-  // console.log()
+
   return (
     <SingleAccessory accessory={accessory} />
   )
 }
 
 const mapState = ({accessories}) => ({accessories})
-// const mapDispatch = () => {
-//   return {
 
-//   }
-// }
 
 const AccessoryContainer = withRouter(connect(mapState)(Accessory))
 export default AccessoryContainer
