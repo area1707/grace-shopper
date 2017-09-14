422d36dddd565896c47ad1493bd447ec4a71dff2
Rasprit Kaur <kaur.rasprit@gmail.com>
2017-09-14 14:17:54 -0400

    test: created fakeUser for comp testing

--- tests/components/UserProfile.spec.js
+++ tests/components/UserProfile.spec.js
@@ -1,13 +1,18 @@
-import {expect} from 'chai'
+import { expect } from 'chai'
 import React from 'react'
-import {shallow} from 'enzyme'
-import {UserProfile} from '../../client/components/UserProfile'
+import { shallow } from 'enzyme'
+import { UserProfile } from '../../client/components/UserProfile'
 
 describe('UserProfile', () => {
   let userProfile
 
   beforeEach('Create user component', () => {
-    userProfile = shallow(<UserProfile user={{name:'Bob', email:'bob@bob.com', shipping_address:'5 Hanover'}} />)
+    const fakeUser = {
+      name: 'Bob',
+      email: 'bob@bob.com',
+      shipping_address: '5 Hanover'
+    }
+    userProfile = shallow(<UserProfile user={fakeUser} />)
   })
 
   it('renders the greeting in an h1', () => {
