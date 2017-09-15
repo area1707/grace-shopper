eac765c66f3047f321ec26e74dc014dcec969a56
Rasprit Kaur <kaur.rasprit@gmail.com>
2017-09-14 20:54:22 -0400

    test: can get initial state but failed to add new user to state

--- tests/reducers/user.spec.js
+++ tests/reducers/user.spec.js
@@ -5,23 +5,26 @@ import userReducer from '../../client/reducers/users';
 
 describe('User reducer', () => {
 
-    let testStore;
-    beforeEach('Create testing store', () => {
+    let testStore, fakeUser;
+    beforeEach('Create testing store and fake user', () => {
         testStore = createStore(userReducer);
+        fakeUser = {
+            name: 'Bob',
+            email: 'bob@bob.com',
+            shipping_address: '5 Hanover'
+          }
     });
 
     it('has expected initial state', () => {
-        expect(testStore.getState()).to.be.deep.equal({
-            users: []
-        });
+        expect(testStore.getState()).to.be.deep.equal([]);
     });
 
     describe('CREATE', () => {
 
         it('creates new user and adds to users array', () => {
-            testStore.dispatch({ type: 'CREATE', user: {userObj} });
+            testStore.dispatch({ type: 'CREATE', user: fakeUser });
             const newState = testStore.getState();
-            expect(newState.users).to.be.deep.equal([{userObj}]);
+            expect(newState.users).to.be.deep.equal([fakeUser]);
         });
 
     });
