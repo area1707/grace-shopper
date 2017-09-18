ce7033901eb028a9b35236c4fe1e7536664dd34e
Erin Shaw <erin.kimberly.shaw@gmail.com>
2017-09-18 19:18:14 -0400

    feat: backbone for write review component

---
+++ client/components/WriteReview.jsx
@@ -0,0 +1,65 @@
+import {connect} from 'react-redux'
+import React, {Component} from 'react'
+import {withRouter} from 'react-router'
+import {addReview} from '../reducers/reviews'
+
+function WriteReview(props) {
+
+    const accessory  = props.accessory
+    const stars = (num) => {
+      let reviewStars = []
+      for (var star = 1; star <= num; star++) {
+        reviewStars.push(<img src="/img/star.svg" className="star" key={star} />)
+      }
+      return reviewStars
+    }
+
+    const starRender = (starsArr) => starArr.map(star => star)
+
+    return (
+      <div>
+        <img src={accessory.imageUrl} />
+        <h3>{accessory.name}</h3>
+        <form>
+          <div class="form-group">
+            <label for="exampleFormControlSelect1">Select Rating</label>
+            <select class="form-control" id="exampleFormControlSelect1" >
+              <option>1</option>
+              <option>2</option>
+              <option>3</option>
+              <option>4</option>
+              <option>5</option>
+            </select>
+          </div>
+          <div>
+            {/* {on option value render differnt amt of stars?} */}
+          </div>
+        <div class="form-group">
+          <label for="exampleFormControlTextarea1">What did you think?!</label>
+          <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+        </div>
+      </form>
+      </div>
+    );
+
+
+}
+
+const mapState = ({reviews}) => ({reviews})
+const mapDispatch = (dispatch, ownProps) => {
+  return {
+    handleSubmit: function(event, accessoryId) {
+      //const content
+      //const star
+      //const accessoryId
+      //const userId
+      event.preventDefault()
+      dispatch(addReview(/*add body to submit here*/))
+      ownProps.history.push('/')
+    }
+  }
+}
+export default withRouter(connect(mapState)(SingleReview))
+
+//how to grab accessory id
+//is thunk written to post new review then update store?
