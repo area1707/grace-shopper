dc761dbf4cc20a64ce9609d8d43f63b699303349
Rasprit Kaur <kaur.rasprit@gmail.com>
2017-09-18 18:44:17 -0400

    feat: can access userOrders

--- client/components/UserProfile.jsx
+++ client/components/UserProfile.jsx
@@ -1,29 +1,49 @@
-import React from 'react'
-import { withRouter } from 'react-router-dom'
+import React, { Component } from 'react'
+import { withRouter, NavLink } from 'react-router-dom'
 import { connect } from 'react-redux'
 
-function UserProfile(props) {
+class UserProfile extends Component {
+    constructor(props){
+        super(props)
 
-        const { user } = props
+    }
+
+    componentDidMount(){
+      //const userOrders = this.props.orderAccessories.orders.filter(order => order.userId == this.props.user.id)
+      //console.log('hope this wors', userOrders)
+      //console.log('hi', this.props.orderAccessories[0].order)
+    }
+
+    render(){  
+        const { user, orderAccessories } = this.props
+        const userOrders = orderAccessories.length ? orderAccessories.filter(orderAcc => orderAcc.order.userId == user.id) : {}   
+        console.log('wtf', userOrders)
         return (
             <div>
-                {!!user && 
+                {user && 
                 <div>
                     <h1>Hello, {user.name}!</h1>
                     <h3>Email: {user.email}</h3>
                     <h3>Shipping Address: {user.shipping_address}</h3>
+                    <h3>Order History: </h3>
+
+                    { /*MAP THRU ORDERACCESSORIES.ORDER 
+                    WHERE ORDER.USERID == MATCH PARAM*/}
                 </div>
                 }
             </div>
         )
     }
-
+}
 
 const mapStateToProps = function (state, ownProps) {
     const userId = Number(ownProps.match.params.userId)
     const selectedUser = state.users.filter(user => user.id == userId)[0]
+    const orderAcc = state.orderAccessories
+
     return {
-        user: selectedUser
+        user: selectedUser,
+        orderAccessories: orderAcc
     }
 }
 
