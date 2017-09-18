1849e451cabe64069eaf2bf3cade58e73f2d3823
Rasprit Kaur <kaur.rasprit@gmail.com>
2017-09-18 12:55:08 -0400

    fix: grab all accessories matching orderId

--- client/components/SingleOrder.jsx
+++ client/components/SingleOrder.jsx
@@ -9,22 +9,24 @@ class SingleOrder extends Component{
     
     render() {
       const { orderAcc } = this.props
-      console.log('my ORDER OBJ', orderAcc)
+      const order = orderAcc.length ? orderAcc[0].order: {}
+
     return (
       <div className="list-group-item min-content students-item">
-        {orderAcc && <div className="media" className="media-body">
+        {orderAcc.length && <div className="media" className="media-body">
+            {console.log('my ORDER OBJ', orderAcc)}
             <h4 className="media-heading tucked">
-              <span placeholder="order id here">Order #{orderAcc.order.id}</span>
+              <span placeholder="order id here">Order #{order.id}</span>
             </h4>
             <p className="">
-              <span placeholder="order created">Order placed on: {orderAcc.createdAt}</span>
+              <span placeholder="order created">Order placed on: {order.createdAt}</span>
             </p>
             <p className="">
-              <span placeholder="status">Status: {orderAcc.order.status}</span>
+              <span placeholder="status">Status: {order.status}</span>
             </p>
-            <p className="">
+            {/*<p className="">
               <span placeholder="accessoryNameQuantity">${orderAcc.accessory.accessoryId} ${orderAcc.accessory.quantity} ACCESSORY_NAME</span>
-            </p>
+    </p>*/}
             <p className="">
               <span placeholder="10">Total Cost: {orderAcc.orderedPrice}</span>
             </p>
@@ -37,9 +39,10 @@ class SingleOrder extends Component{
 
 const mapState = function (state, ownProps) {
   const orderId = Number(ownProps.match.params.orderId)
-  const selectedOrder = state.orderAccessories.filter(order => order.order.id == orderId)[0]
+  const selectedOrderAcc = state.orderAccessories.filter(orderAcc => orderAcc.order.id == orderId)
+  console.log(selectedOrderAcc)
   return {
-      orderAcc: selectedOrder      
+      orderAcc: selectedOrderAcc
   }
 }
 
