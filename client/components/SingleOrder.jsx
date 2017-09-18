f9207539779f158687969301f92366c02279fa32
Rasprit Kaur <kaur.rasprit@gmail.com>
2017-09-18 16:14:59 -0400

    fix: add $ to total cost

--- client/components/SingleOrder.jsx
+++ client/components/SingleOrder.jsx
@@ -30,7 +30,7 @@ function SingleOrder(props){
           }
           </ul>
           <p className="">
-          <span placeholder="10">Total Cost: {total}</span>
+          <span placeholder="10">Total Cost: ${total}</span>
           </p>
         
         </div>
