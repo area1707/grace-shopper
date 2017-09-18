6470fa5fb0249245def8a98e8bccd7663051ea51
Rasprit Kaur <kaur.rasprit@gmail.com>
2017-09-18 14:55:21 -0400

    feat: render singleAccessory comps inside order

--- client/components/SingleOrder.jsx
+++ client/components/SingleOrder.jsx
@@ -1,20 +1,20 @@
 import {connect} from 'react-redux'
 import React, {Component} from 'react'
 import {withRouter} from 'react-router'
-import {fetchOrders} from '../reducers/orders'
-import store from '../store'
+import SingleAccessory from './SingleAccessory'
 
 
 function SingleOrder(props){
   const { orderAccs } = props
   const order = orderAccs.length ? orderAccs[0].order: {}
+  let total = 0;
 
   return (
     <div>
       {orderAccs.length &&
         <div>
         <h2>Order #{order.id}</h2>
-        <div className="list-group-item min-content media media-body"> 
+        <div className="list-group-item min-content media "> 
           {console.log('my ORDER OBJ', orderAccs)}
           <p className="">
             <span placeholder="order created">Order placed on: {order.createdAt}</span>
@@ -24,17 +24,17 @@ function SingleOrder(props){
           </p>
           <ul>
           {orderAccs.map(orderAcc => {
+            {total+=orderAcc.orderedPrice}
             return(
-                <li key = {orderAcc.accessoryId}>
-                {orderAcc.quantity} {orderAcc.accessory.name}
-                </li>
+                <SingleAccessory key={orderAcc.accessoryId} accessory={orderAcc.accessory} />
+
               
             )
           })
           }
           </ul>
           <p className="">
-          <span placeholder="10">Total Cost: {orderAccs.orderedPrice}</span>
+          <span placeholder="10">Total Cost: {total}</span>
           </p>
         
         </div>
