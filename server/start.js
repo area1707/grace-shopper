ab1388a02dbddd158da724d7aa857be0540af925
An <lean257@gmail.com>
2017-09-19 18:02:05 -0400

    Update start.js

--- server/start.js
+++ server/start.js
@@ -1,9 +1,10 @@
 'use strict'
 const db = require('./db/index')
 const app = require('./middleware')
+const port = process.env.PORT || 5000
 
 db.sync({})  // sync our database
 .then(function(){
-    app.listen(5000) // then start listening with our express server once we have synced
+    app.listen(port) // then start listening with our express server once we have synced
     console.log('listening to 5000')
 })
