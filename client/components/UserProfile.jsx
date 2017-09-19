2ee54086fef9dcbc09ab7014b387642bb185beb0
Rasprit Kaur <kaur.rasprit@gmail.com>
2017-09-18 23:54:10 -0400

    feat: render order history in user profile

--- client/components/UserProfile.jsx
+++ client/components/UserProfile.jsx
@@ -1,40 +1,35 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { withRouter, NavLink } from 'react-router-dom'
 import { connect } from 'react-redux'
 
-class UserProfile extends Component {
-    constructor(props){
-        super(props)
-
-    }
-
-    componentDidMount(){
-      //const userOrders = this.props.orderAccessories.orders.filter(order => order.userId == this.props.user.id)
-      //console.log('hope this wors', userOrders)
-      //console.log('hi', this.props.orderAccessories[0].order)
-    }
-
-    render(){  
-        const { user, orderAccessories } = this.props
-        const userOrders = orderAccessories.length ? orderAccessories.filter(orderAcc => orderAcc.order.userId == user.id) : {}   
-        console.log('wtf', userOrders)
-        return (
+function UserProfile(props) {
+    const { user, orderAccessories } = props
+    const userOrderAccs = orderAccessories.length ? orderAccessories.filter(orderAcc => orderAcc.order.userId == user.id) : {}  
+    //let userOrders = userOrderAccs.length? uniqBy(userOrders,'orderId') : []
+    return (
+        <div>
+            {user && 
             <div>
-                {user && 
-                <div>
-                    <h1>Hello, {user.name}!</h1>
-                    <h3>Email: {user.email}</h3>
-                    <h3>Shipping Address: {user.shipping_address}</h3>
-                    <h3>Order History: </h3>
+                <h1>Hello, {user.name}!</h1>
+                <h3>Email: {user.email}</h3>
+                <h3>Shipping Address: {user.shipping_address}</h3>
+                <h3>Order History: </h3>
 
-                    { /*MAP THRU ORDERACCESSORIES.ORDER 
-                    WHERE ORDER.USERID == MATCH PARAM*/}
-                </div>
-                }
+                {Array.isArray(userOrderAccs) && userOrderAccs.map(userOrderAcc => {
+                    console.log('wtf', userOrderAcc.orderId)
+                    return(
+                        <NavLink to = {`/orders/${userOrderAcc.orderId}`} key={userOrderAcc.orderId}>
+                        Order#{userOrderAcc.orderId}____Placed On: {userOrderAcc.order.createdAt.slice(0,10)}____Status:{userOrderAcc.order.status}
+                        <br/>
+                        </NavLink>
+                    )
+                })}
             </div>
-        )
-    }
+            }
+        </div>
+    )
 }
+
 
 const mapStateToProps = function (state, ownProps) {
     const userId = Number(ownProps.match.params.userId)
