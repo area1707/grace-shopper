c74cf16a249e056e96b8c66f1005e823be5e03e8
Alice Chen <alicejychen15@gmail.com>
2017-09-19 16:20:43 -0400

    feat: allorders component, JUST STARTED

---
+++ client/components/AllOrders.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import store from '../store';
+import { withRouter, NavLink } from 'react-router-dom'
+import { connect } from 'react-redux'
+
+export class AllOrders extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <div>
+      </div>
+    )
+  }
+}
