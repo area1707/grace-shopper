5b84ea2954280615c613e48f89e7f391d1997fdc
Alice Chen <alicejychen15@gmail.com>
2017-09-19 11:09:31 -0400

    fix: linking user profiles to page

--- client/components/AllUsers.jsx
+++ client/components/AllUsers.jsx
@@ -4,3 +4,47 @@ import { withRouter } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { fetchUsers } from '../reducers/users'
 
+export class AllUsers extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  // componentDidMount() {
+  //   store.dispatch(fetchUsers())
+  // }
+
+  render() {
+    console.log('WHERE MA USERS AT', this.props)
+    const { users } = this.props
+    return (
+      <div>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Shipping Address</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">1</th>
+              <td>Mark</td>
+              <td>Otto</td>
+              <td>@mdo</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = function (state, ownProps) {
+  return {
+      users: state.users
+  }
+}
+
+export default withRouter(connect(mapStateToProps)(AllUsers));
